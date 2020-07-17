1e698cb1a7e24fa812ba29d00d3552bf9f273008
elvismariel <elvismariel@gmail.com>
2020-07-17 17:22:04 -0300

    Criacao do componente de Nivel de dificuldade

---
+++ src/components/level.vue
@@ -0,0 +1,26 @@
+<template>
+    <div class="hello">
+        <div class="custom-control custom-radio custom-control-inline">
+            <input type="radio" id="customRadioInline1" name="customRadioInline1" class="custom-control-input">
+            <label class="custom-control-label" for="customRadioInline1">Fácil</label>
+        </div>
+        <div class="custom-control custom-radio custom-control-inline">
+            <input type="radio" id="customRadioInline2" name="customRadioInline1" class="custom-control-input">
+            <label class="custom-control-label" for="customRadioInline2">Médio</label>
+        </div>
+        <div class="custom-control custom-radio custom-control-inline">
+            <input type="radio" id="customRadioInline3" name="customRadioInline1" class="custom-control-input">
+            <label class="custom-control-label" for="customRadioInline3">Difícil</label>
+        </div>
+    </div>
+</template>
+
+<script>
+export default {
+    name: 'Level'
+}
+</script>
+
+<style scoped>
+
+</style>
